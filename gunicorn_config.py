2bb645b07974cce73339e51c975af067a695b77b
amarad21-1706 <126468216+amarad21-1706@users.noreply.github.com>
2024-06-14 10:14:17 +0200

    third gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,8 @@
 import os
 import multiprocessing
 
-bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"  # Default to port 8000 if $PORT is not set
-workers = multiprocessing.cpu_count()  # Adjust the number of workers
+bind = f"0.0.0.0:{os.environ.get('PORT', '10000')}"  # Default to port 8000 if $PORT is not set
+workers = 1
 threads = 1  # Start with a single thread
 timeout = 200
 loglevel = 'debug'
